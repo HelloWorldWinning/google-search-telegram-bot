86681ac2eb442e5ca494c51877513d4547d882ab
root <feedzhulei@aol.com>
2022-12-11 01:42:30 +0800

    new

--- src/app/lazy.py
+++ src/app/lazy.py
@@ -1,4 +1,5 @@
 # See: http://stackoverflow.com/questions/3012421/python-lazy-property-decorator
+# turn  method  to  attr
 class Lazy(object):
 	def __init__(self, lazy_init):
 		self._lazy_init = lazy_init
